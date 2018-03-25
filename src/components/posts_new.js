32617e7c76fa6a0aba9a912aaed68a38742fde2d
Loan Nguyen <nguyenloan150492@gmail.com>
2018-03-26 01:26:24 +0300

    add bootstrap - form validate

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,40 +1,68 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
+import { FormGroup, ControlLabel, FormControl, HelpBlock , Button } from 'react-bootstrap';
 
 class PostsNew extends Component {
 
     renderField(field) {
+        const { meta } = field;
         return (
-            <div>
-                <label>Title</label>
-                <input type="text" {...field.input} />
-            </div>
+            <FormGroup validationState={meta.touched && meta.error ? "error" : null}>
+                <ControlLabel>{field.label}</ControlLabel>
+                <FormControl type="text"  {...field.input} />
+                <HelpBlock >
+                    {meta.touched ? meta.error : ''}
+                </HelpBlock>
+            </FormGroup>
         );
     }
 
+    onSubmit(values) {
+
+    }
+
     render() {
+        const { handleSubmit } = this.props;
         return (
-            <form >
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
                     label="Title"
                     name="title"
                     component={this.renderField} />
                 <Field
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField} />
                 <Field
                     label="Content"
                     name="content"
                     component={this.renderField} />
 
-                <button type="submit">Submit</button>
+                 <Button bsStyle="info" type="submit">Submit</Button>
             </form>
         );
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    if (!values.title) {
+        errors.title = "Enter a title";
+    }
+    if (!values.categories) {
+        errors.categories = "Enter some categories";
+    }
+    if (!values.content) {
+        errors.content = "Enter some content";
+    }
+
+    return errors;
+
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
